facae38558285b525c27cd755e1236e3c8178d9a
Juan <juandiegorodriguezcarreno@gmail.com>
2026-09-09 11:23:40 -0600

    add 1.5 commit

--- dmit2015-javase-demo/src/main/java/dmit2015/Main.java
+++ dmit2015-javase-demo/src/main/java/dmit2015/Main.java
@@ -1,9 +1,7 @@
 package dmit2015;
 
-import java.awt.desktop.SystemEventListener;
-
 public class Main {
     public static void main (String[] args){
-        system.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
