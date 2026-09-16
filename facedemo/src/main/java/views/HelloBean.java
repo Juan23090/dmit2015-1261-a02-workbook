2dea8111c8f53aceedb0f7733c21eb1db49b357e
Juan <juandiegorodriguezcarreno@gmail.com>
2026-09-16 11:24:05 -0600

    Finished the exercise 4B adding first name and last name

--- facedemo/src/main/java/views/HelloBean.java
+++ facedemo/src/main/java/views/HelloBean.java
@@ -25,7 +25,7 @@ public class HelloBean {
     public String onSubmit() {
 
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("FacesMessage from java part" + userInput));
-        userInput = null;
+        //userInput = null;
         return null;
     }
 }
